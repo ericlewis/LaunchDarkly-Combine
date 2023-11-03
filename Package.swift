7c46778def12786a748797c053f0dd04117c41da
Marek Zvara <marek724@gmail.com>
2023-11-03 10:49:20 +0100

    Update min iOS version to 15

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LaunchDarkly+Combine",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v15),
         .macOS(.v10_15),
         .watchOS(.v6),
         .tvOS(.v13)
